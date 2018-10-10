a2cbfa767c9775b5ca5d48d19d5842fd2d789a1a
vitaliyvolkov <rq.volkov@gmail.com>
2018-10-10 18:28:33 +0300

    Contact us admin email notification

--- user/tests/test_views.py
+++ user/tests/test_views.py
@@ -1,12 +1,16 @@
+from unittest.mock import patch
+
 from django.urls import reverse
 from rest_framework import status
 
 from common.tests import BaseViewTests
 from common.utils import random_string, random_email
+from user.models import ContactForm
 
 
 class SubscribeListViewTests(BaseViewTests):
-    def test_create(self):
+    @patch('user.tasks.new_subscribe_list_mail.delay')
+    def test_create(self, mock_obj):
         url = reverse('subscribe-list-list')
         data = {'email': random_email()}
 
@@ -14,10 +18,13 @@ class SubscribeListViewTests(BaseViewTests):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['email'], data['email'])
         self.assertIsNotNone(response.data.get('date_created'))
+        recipients = [data['email']]
+        mock_obj.assert_called_once_with(recipients)
 
 
 class ContactFormViewTests(BaseViewTests):
-    def test_create(self):
+    @patch('user.tasks.contact_us_admin_mail.delay')
+    def test_create(self, mock_obj):
         url = reverse('contact-form-list')
         data = {'email': random_email(), 'name': random_string(), 'message': random_string()}
 
@@ -27,3 +34,5 @@ class ContactFormViewTests(BaseViewTests):
         self.assertEqual(response.data['name'], data['name'])
         self.assertEqual(response.data['message'], data['message'])
         self.assertIsNotNone(response.data.get('date_created'))
+        created_pk = ContactForm.objects.get(email=data['email']).pk
+        mock_obj.assert_called_once_with(created_pk)
